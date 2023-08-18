9fcce8715d8341f5b8040291cdcbf6c66577739b
CultreLi <448931207@qq.com>
2023-08-18 21:20:48 +0800

    fix:deleta unused files

--- Assets/GameLauncher/Editor/Misc/Type.cs
+++ Assets/GameLauncher/Editor/Misc/Type.cs
@@ -12,53 +12,16 @@ namespace UnityGameFramework.Editor
     {
         private static readonly string[] AssemblyNames =
         {
-#if UNITY_2017_3_OR_NEWER
             "GameEngine.Runtime","GameLauncher.Runtime","GameMain.Runtime",
-#endif
             "Assembly-CSharp"
         };
 
         private static readonly string[] EditorAssemblyNames =
         {
-#if UNITY_2017_3_OR_NEWER
             "GameEngine.Editor","GameLauncher.Editor","GameMain.Editor",
-#endif
             "Assembly-CSharp-Editor"
         };
 
-        /// <summary>
-        /// 获取配置路径。
-        /// </summary>
-        /// <typeparam name="T">配置类型。</typeparam>
-        /// <returns>配置路径。</returns>
-        internal static string GetConfigurationPath<T>() where T : ConfigPathAttribute
-        {
-            foreach (System.Type type in Utility.Assembly.GetTypes())
-            {
-                if (!type.IsAbstract || !type.IsSealed)
-                {
-                    continue;
-                }
-
-                foreach (FieldInfo fieldInfo in type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly))
-                {
-                    if (fieldInfo.FieldType == typeof(string) && fieldInfo.IsDefined(typeof(T), false))
-                    {
-                        return (string)fieldInfo.GetValue(null);
-                    }
-                }
-
-                foreach (PropertyInfo propertyInfo in type.GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly))
-                {
-                    if (propertyInfo.PropertyType == typeof(string) && propertyInfo.IsDefined(typeof(T), false))
-                    {
-                        return (string)propertyInfo.GetValue(null, null);
-                    }
-                }
-            }
-
-            return null;
-        }
 
         /// <summary>
         /// 获取指定基类的所有子类的名称。
